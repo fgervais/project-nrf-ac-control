06a628e0ba3f5f0408597928901780c38c74b5bc
Francois Gervais <francoisgervais@gmail.com>
2023-04-17 12:14:18 -0400

    Read the tmp117

--- app/src/main.c
+++ app/src/main.c
@@ -1,4 +1,5 @@
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/pm/device.h>
 
@@ -17,14 +18,41 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #define SLEEP_TIME_MS   10
 #define LED0_NODE DT_ALIAS(myled0alias)
 
+#define TMP116_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(ti_tmp116)
+
+
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+
+double get_current_temperature(const struct device *const dev)
+{
+	int ret;
+	struct sensor_value temp_value;
+
+	ret = sensor_sample_fetch(dev);
+	if (ret) {
+		LOG_ERR("Failed to fetch measurements (%d)", ret);
+		return 0;
+	}
+
+	ret = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP,
+				 &temp_value);
+	if (ret) {
+		LOG_ERR("Failed to get measurements (%d)", ret);
+		return 0;
+	}
+
+	return sensor_value_to_double(&temp_value);
+}
 
 void main(void)
 {
 	int ret;
+	double current_temp;
+
 	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 	const struct device *pwm0 = DEVICE_DT_GET(DT_NODELABEL(pwm0));
+	const struct device *const tmp117 = DEVICE_DT_GET(TMP116_NODE);
 
 
 	if (app_event_manager_init()) {
@@ -42,9 +70,17 @@ void main(void)
 		return;
 	}
 
+	__ASSERT(device_is_ready(tmp117), "TMP117 device not ready");
+	LOG_INF("Device %s - %p is ready", tmp117->name, tmp117);
+
 	while(1) {
+		LOG_INF("────────────────────────────────────────");
+		current_temp = get_current_temperature(tmp117);
+
+		LOG_INF("🌡️ temp is %g°C", current_temp);
+
 		drv_ir_send_on(pwm0);
-		k_sleep(K_SECONDS(1));
+		k_sleep(K_SECONDS(5));
 	}
 
 	LOG_INF("****************************************");
