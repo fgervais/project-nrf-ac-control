d3496804741072c1b3286d8905b0c6fadef59950
Francois Gervais <francoisgervais@gmail.com>
2023-04-19 18:26:54 -0400

    Prepare HA discovery structs

--- app/src/ha.c
+++ app/src/ha.c
@@ -26,11 +26,83 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(home_assistant, LOG_LEVEL_DBG);
 
+#include <zephyr/data/json.h>
 #include <zephyr/drivers/hwinfo.h>
 
 
-#define DEVICE_ID_BYTE_SIZE 8
-static int get_device_id(uint64_t *device_id)
+#define DEVICE_ID_BYTE_SIZE	8
+
+
+struct device {
+	const char identifiers[2 * DEVICE_ID_BYTE_SIZE + 1];
+	const char *name;
+	const char *sw_version;
+	const char *hw_version;
+	const char *model;
+	const char *manufacturer;
+};
+
+struct config {
+	const char *base_path;
+	const char *name;
+	float initial;
+	float min_temp;
+	float max_temp;
+	const char *modes[2];
+	int number_of_modes;
+	const char *mode_command_topic;
+	const char *mode_command_template;
+	const char *temperature_command_topic;
+	float precision;
+	struct device dev;
+};
+
+
+static struct config ac_config = {
+	.base_path = "home/room/kitchen/climate/ac",
+	.name = "Split Air Conditioner",
+	.initial = 22,
+	.min_temp = 16,
+	.max_temp = 30,
+	.modes = { [ "off", "cool" ] },
+	.number_of_modes = 2,
+	.mode_command_topic = "~/mode/set",
+	.mode_command_template = "{{ value if value=='off' else 'on' }}",
+	.temperature_command_topic = "~/temperature/set",
+	.precision = 1.0,
+	.dev = {
+		// .identifiers = ,
+		.name = "Split Air Conditioner",
+		.sw_version = "7cdc3fb1ddce7a31e6192e2760791e24a2d11d41",
+		.hw_version = "1.0.0",
+		.model = "unknown",
+		.manufacturer = "GREE ELECTRIC APPLIANCES INC.",	
+	},
+};
+
+static const struct json_obj_descr device_descr[] = {
+	JSON_OBJ_DESCR_PRIM(struct device, unit,	 JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct device, identifiers,	 JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct device, name,	 JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct device, sw_version,	 JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct device, hw_version,	 JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct device, model,	 JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct device, manufacturer, JSON_TOK_STRING),
+};
+
+static const struct json_obj_descr config_descr[] = {
+	JSON_OBJ_DESCR_PRIM(struct config, base_path,			JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct config, name,			JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct config, initial,			JSON_TOK_FLOAT),
+	JSON_OBJ_DESCR_PRIM(struct config, min_temp,			JSON_TOK_FLOAT),
+	JSON_OBJ_DESCR_PRIM(struct config, max_temp,			JSON_TOK_FLOAT),
+	JSON_OBJ_DESCR_ARRAY(struct config, modes, 2, number_of_modes,	JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct config, mode_command_topic,		JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct config, mode_command_template,	JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct config, temperature_command_topic,	JSON_TOK_STRING),
+	JSON_OBJ_DESCR_PRIM(struct config, precision,			JSON_TOK_FLOAT),
+	JSON_OBJ_DESCR_OBJECT(struct config, dev, device_descr),
+};
 {
 	uint8_t dev_id[DEVICE_ID_BYTE_SIZE];
 	ssize_t length;
